fe8468afb0b04f437d7224ae3cb5acb04a037e52
HariDhaker <hariprasaddhakar0@gmail.com>
2025-05-31 18:49:34 +0530

    31 may

--- Sorting/quick.java
+++ Sorting/quick.java
@@ -1,12 +1,39 @@
 class QuickSort{
-    public static void main(Strin args[])
+    static int partition(int arr[],int st,int end)
     {
-        
+        int pvt=arr[end];
+        int idx=st-1;
+        for(int i=st;i<end;i++)
+        {
+           if(pvt>=arr[i])
+           {
+            idx++;
+            int temp = arr[i];
+            arr[i]=arr[idx];
+            arr[idx]=temp;
+           }
+        }
+        idx++;
+        int temp = arr[end];
+        arr[end]=arr[idx];
+        arr[idx]=temp;
+        return idx;
     }
+    static void quickSort(int arr[],int st,int end)
+    {
+        if(st>end)
+        {
+            return;
+        }
+        int pvtIndx=partition(arr,st,end);
+        quickSort(arr,st,pvtIndx-1);
+        quickSort(arr,pvtIndx+1,end);
+    }
+    public static void main(String args[])
     {
         int[] array={4,2,3,5,6};
        int n = 5;
-       quickSort(array,5);
+       quickSort(array,0,array.length-1);
        System.out.println("Sorted array : ");
        for(int i=0;i<n;i++)
        {
